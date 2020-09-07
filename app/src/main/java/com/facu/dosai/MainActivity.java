798d2eea1a25bc3c363da032df2cdf8fa7e9c271
Gustavo <gustavonascimento_31@hotmail.com>
2020-09-07 13:50:43 -0300

    0.2

--- app/src/main/java/com/facu/dosai/MainActivity.java
+++ app/src/main/java/com/facu/dosai/MainActivity.java
@@ -3,6 +3,7 @@ package com.facu.dosai;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,6 +21,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edtConcentracao = (EditText) findViewById(R.id.edtConcentracao);
+        edtDose = (EditText) findViewById(R.id.edtConcentracao);
+        edtPeso = (EditText) findViewById(R.id.edtPeso);
+        btnCalcular = (Button) findViewById(R.id.btnCalcular);
+        txvResultado = (TextView) findViewById(R.id.txvResultado);
+    }
+
+    public void calcular (View view){
+
+        double doseStr = Double.valueOf(edtDose.getText().toString());
+        double pesoStr = Double.valueOf(edtPeso.getText().toString());
+        double concentracaoStr = Double.valueOf(edtConcentracao.getText().toString());
+
+        double result = (pesoStr * doseStr) / concentracaoStr;
+
+        txvResultado.setText(result);
 
     }
 }
